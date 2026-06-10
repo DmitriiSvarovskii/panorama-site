e89275ce7d402aba9380dfa743ba3975cc1db974
Dmitrii <swarovskidima@gmail.com>
2026-06-11 00:07:37 +0300

    добавил homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
 import PresentationPage from './pages/PresentationPage'
-
+import { Navigate } from 'react-router-dom'
 function App() {
   return (
     <BrowserRouter>
+
+
       <Routes>
+
         <Route path="/" element={<HomePage />} />
+
         <Route path="/presentation" element={<PresentationPage />} />
+
+        <Route
+
+          path="*"
+
+          element={<Navigate to="/" replace />}
+
+        />
+
       </Routes>
     </BrowserRouter>
   )
