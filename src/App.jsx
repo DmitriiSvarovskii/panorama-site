767fdd012e4773327d69c334039e493209c1852c
Dmitrii <swarovskidima@gmail.com>
2026-06-11 00:22:22 +0300

    Add redirect for unknown routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
 import PresentationPage from './pages/PresentationPage'
-import { Navigate } from 'react-router-dom'
+
 function App() {
   return (
     <BrowserRouter>
-
-
       <Routes>
-
         <Route path="/" element={<HomePage />} />
-
         <Route path="/presentation" element={<PresentationPage />} />
-
-        <Route
-
-          path="*"
-
-          element={<Navigate to="/" replace />}
-
-        />
-
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
